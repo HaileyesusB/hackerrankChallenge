66470c4b9e20a5713e3c94cf9262088d27fe0bca
hbelew@excellerentsolutions.com <hbelew@excellerentsolutions.com>
2022-06-28 16:02:35 +0300

    Updates

---
+++ AVeryBigSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/*
+       * Complete the 'aVeryBigSum' function below.
+       *
+       * The function is expected to return a LONG_INTEGER.
+       * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+       */
+
+namespace hackerrankChallenge
+{
+    class AVeryBigSum
+    {
+    }
+
+
+
+class Results
+    {
+
+      
+
+        public static long aVeryBigSum(List<long> ar)
+        {
+            return 0;
+        }
+
+    }
+
+    class Solutions
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+            long result = Results.aVeryBigSum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
---
+++ CompareTheTriplets.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+namespace hackerrankChallenge
+{
+    class CompareTheTriplets
+    {
+
+
+    }
+
+class Result
+    {
+
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int aliceScore = 0, bobScore = 0, totalTests = 3;
+
+            for (int i = 0; i < totalTests; i++)
+            {
+
+                aliceScore += a[i] > b[i] ? 1 : 0;
+
+                bobScore += a[i] < b[i] ? 1 : 0;
+            }
+
+            return new List<int> { aliceScore, bobScore };
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = Result.compareTriplets(a, b);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
+/*
+ * Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given a and b, determine their respective comparison points.
+
+Example
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+For elements *0*, Bob is awarded a point because a[0] .
+For the equal elements a[1] and b[1], no points are earned.
+Finally, for elements 2, a[2] > b[2] so Alice receives a point.
+The return array is [1, 1] with Alice's score first and Bob's second.
+
+Function Description
+
+Complete the function compareTriplets in the editor below.
+
+compareTriplets has the following parameter(s):
+
+int a[3]: Alice's challenge rating
+int b[3]: Bob's challenge rating
+Return
+
+int[2]: Alice's score is in the first position, and Bob's score is in the second.
+Input Format
+
+The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+
+Constraints
+
+1 ≤ a[i] ≤ 100
+1 ≤ b[i] ≤ 100
+Sample Input 0
+
+5 6 7
+3 6 10
+Sample Output 0
+
+1 1
+Explanation 0
+
+In this example:
+
+Now, let's compare each individual score:
+
+, so Alice receives  point.
+, so nobody receives a point.
+, so Bob receives  point.
+Alice's comparison score is , and Bob's comparison score is . Thus, we return the array .
+
+Sample Input 1
+
+17 28 30
+99 16 8
+Sample Output 1
+
+2 1
+Explanation 1
+
+Comparing the  elements,  so Bob receives a point.
+Comparing the  and  elements,  and  so Alice receives two points.
+The return array is .
+ * 
+ * 
+ * 
+ */
+
+
